a9ee4f676f118c28633dda118f630fa3aa8d072d
tta <toni.tannous@isae.edu.lb>
2017-08-27 15:55:52 +0300

    Send Mail Component
    this is a send mail component that could be used to send notification
    from the workflow

---
+++ src/main/java/cls/mail/SendEmailManager.java
@@ -0,0 +1,90 @@
+package cls.mail;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendEmailManager {
+	public static void sendMailWithoutAttachement(EmailConfiguration emailConfig, EmailWrapper[] emailsTo) 
+	{	 
+		//<dependency>
+	//	<groupId>com.sun.mail</groupId>
+	//	<artifactId>javax.mail</artifactId>
+//		<version>1.5.5</version>
+//	</dependency>
+	      // Sender's email ID needs to be mentioned
+	      String emailFrom = emailConfig.emailFrom;
+	       String username = emailConfig.userName;
+	       String password =emailConfig.password;
+
+	      // Assuming you are sending email through relay.jangosmtp.net
+	      String host =emailConfig.host;
+
+	      Properties props = new Properties();
+	      props.put("mail.smtp.auth", "true");
+	      props.put("mail.smtp.starttls.enable", "true");
+	      props.put("mail.smtp.host", host);
+	      props.put("mail.smtp.port", emailConfig.port);
+	      //props.put("mail.smtp.port", "587");
+	   // Recipient's email ID needs to be mentioned.
+	      for (EmailWrapper emailTo : emailsTo) {
+			
+		
+	      String to =emailTo.EmailTo;
+	   // Get the Session object.
+	      Session session = Session.getInstance(props,
+	      new javax.mail.Authenticator() {
+	         protected PasswordAuthentication getPasswordAuthentication() {
+	            return new PasswordAuthentication(username, password);
+	         }
+	      });
+
+	      try {
+	         // Create a default MimeMessage object.
+	         Message message = new MimeMessage(session);
+	         // Set From: header field of the header.
+	         message.setFrom(new InternetAddress(emailConfig.emailFrom));
+
+	         // Set To: header field of the header.
+	         message.setRecipients(Message.RecipientType.TO,
+	         InternetAddress.parse(to));
+
+	         // Set Subject: header field
+	         message.setSubject(emailTo.EmailSubject);
+
+	         // Now set the actual message
+	         message.setText(emailTo.EmailBody);
+
+	         // Send message
+	         Transport.send(message);
+
+	         System.out.println("Sent message successfully....");
+	      
+	      
+	      } catch (MessagingException e) {
+	            throw new RuntimeException(e);
+	      }
+	      }
+	}
+	class EmailWrapper
+	{
+		public String EmailTo ;
+		public String EmailSubject ;
+		public String EmailBody;	
+	}
+	class EmailConfiguration
+	{
+		public String host ;
+		public String port ;
+		public Boolean isAuthEnable ;
+		public String emailFrom ;
+		public String userName ;
+		public String password ;
+			
+	}
+}
