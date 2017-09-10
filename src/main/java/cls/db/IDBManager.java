d4db7582a62cd4c628c382e1ad6f427cbb1f1f02
tta <toni.tannous@isae.edu.lb>
2017-09-10 09:24:24 +0300

    Connection Manager
    this is to connect to the database
    mySqlDbManager.getInstance().InsertStatement("INSERT INTO ticks VALUES
    (now())");
    ResultSet rs=mySqlDbManager.getInstance().SelectStatement("SELECT tick
    FROM ticks");

---
+++ src/main/java/cls/db/IDBManager.java
@@ -0,0 +1,8 @@
+package cls.db;
+import java.sql.ResultSet;
+
+public interface IDBManager {
+   	public  int  InsertStatement( String sqlInsertStatement);
+	public  int updateStatement( String sqlUpdateStatement);
+	public  ResultSet SelectStatement( String sqlSelect);
+}
